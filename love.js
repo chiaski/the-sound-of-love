1f1ea2052a8e438d14a2400c98864c04b7e11ee9
Chia Amisola <chiamisola@gmail.com>
2022-02-12 02:03:48 -0500

    lol

---
+++ love.js
@@ -0,0 +1,13 @@
+
+
+let love = [
+  {
+    "c": `Told this girl that I love her, and she said she had the same feelings towards me. We held each other and kissed, life has never been so perfect.
+
+A few days later she said that we shouldn't date.
+
+This song is every emotion ever felt in that time-fram;, both pure ecstasy and pure heartbreak.`, 
+    "video": "iB7E1D_3Na4"
+  }
+  
+]
