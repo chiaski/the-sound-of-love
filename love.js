8bfad834baa639cb727263ea44fa5cafcf45e9ed
Chia Amisola <chiamisola@gmail.com>
2022-02-12 13:06:38 -0500

    sizing fixes, interpunct

--- love.js
+++ love.js
@@ -1002,35 +1002,35 @@ $("body").click(function (e) {
           $(".s").css("opacity", 1);
           $("*").css("cursor", "pointer");
           $("#player").css("opacity", 1);
-        $("body").css("pointer-events", "none !important");
+          $("body").css("pointer-events", "none !important");
           $("p.isaid").fadeIn(3000);
 
           $("div._s").fadeOut(function () {
-            $(this).text(love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s).fadeIn(2000);
+    $(this).text(love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s).fadeIn(2000);
           });
-          
-       
-      if ((love[r].c).length <= 80) {
-        $("p.isaid").css("font-size", "2.2em");
-        $("p.isaid").css("letter-spacing", "-3px");
-        $("p.isaid").css("line-height", "80%;");
-      } else if ((love[r].c).length <= 80) {
-        $("p.isaid").css("font-size", "2em");
-        $("p.isaid").css("letter-spacing", "-2.2px");
-      } else if ((love[r].c).length <= 400) {
-        $("p.isaid").css("font-size", "1.6em");
-        $("p.isaid").css("letter-spacing", "-1.6px");
-      } else if ((love[r].c).length <= 1200) {
-        $("p.isaid").css("font-size", "1.2em");
-        $("p.isaid").css("letter-spacing", "-1px");
-      } else {
-        $("p.isaid").css("font-size", "1em");
-        $("p.isaid").css("letter-spacing", "-.5px");
-      }
+
+
+          if ((love[r].c).length <= 80) {
+            $("p.isaid").css("font-size", "2em");
+            $("p.isaid").css("letter-spacing", "-3px");
+            $("p.isaid").css("line-height", "80%;");
+          } else if ((love[r].c).length <= 80) {
+            $("p.isaid").css("font-size", "1.8em");
+            $("p.isaid").css("letter-spacing", "-2.2px");
+          } else if ((love[r].c).length <= 400) {
+            $("p.isaid").css("font-size", "1.6em");
+            $("p.isaid").css("letter-spacing", "-1.6px");
+          } else if ((love[r].c).length <= 1200) {
+            $("p.isaid").css("font-size", "1.2em");
+            $("p.isaid").css("letter-spacing", "-1px");
+          } else {
+            $("p.isaid").css("font-size", "1em");
+            $("p.isaid").css("letter-spacing", "-.5px");
+          }
 
           $("p.isaid").text(love[r].c);
           player.loadVideoById(love[r].v);
-  $("body").css("pointer-events", "auto");
+          $("body").css("pointer-events", "auto");
         }, 4000);
 
     } else {
@@ -1054,7 +1054,7 @@ function thesoundof(r) {
   });
 
   $("div._s").fadeOut(function () {
-    $(this).text(love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s + " " + love[r].s).fadeIn(2000);
+    $(this).text(love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s + " · " + love[r].s).fadeIn(2000);
   });
 
   $("#player").css("opacity", 0);
@@ -1069,11 +1069,11 @@ function thesoundof(r) {
       player.loadVideoById(love[r].v);
 
       if ((love[r].c).length <= 80) {
-        $("p.isaid").css("font-size", "2.2em");
+        $("p.isaid").css("font-size", "2em");
         $("p.isaid").css("letter-spacing", "-3px");
         $("p.isaid").css("line-height", "80%;");
       } else if ((love[r].c).length <= 80) {
-        $("p.isaid").css("font-size", "2em");
+        $("p.isaid").css("font-size", "1.8em");
         $("p.isaid").css("letter-spacing", "-2.2px");
       } else if ((love[r].c).length <= 400) {
         $("p.isaid").css("font-size", "1.6em");
@@ -1092,7 +1092,7 @@ function thesoundof(r) {
   setTimeout(
     function () {
       $("#cover").css("opacity", 0);
-  $("body").css("pointer-events", "auto");
+      $("body").css("pointer-events", "auto");
     }, 2500);
 
 
