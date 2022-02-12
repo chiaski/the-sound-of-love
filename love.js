41b961229b508650a4ea7db8d4deaabc9ca0514c
Chia Amisola <chiamisola@gmail.com>
2022-02-12 04:01:13 -0500

    lol

--- love.js
+++ love.js
@@ -1,5 +1,16 @@
 
 
+/*
+
+FORMS OF LOVE
+heartbreak
+romantic
+familial
+longing
+self
+
+*/
+
 let love = [
   {
     "c": `Told this girl that I love her, and she said she had the same feelings towards me. We held each other and kissed, life has never been so perfect.
@@ -7,7 +18,260 @@ let love = [
 A few days later she said that we shouldn't date.
 
 This song is every emotion ever felt in that time-fram;, both pure ecstasy and pure heartbreak.`, 
-    "video": "iB7E1D_3Na4"
+    "v": "iB7E1D_3Na4",
+    "s": "Ladies and Gentlemen We Are Floating in Space – Spiritualized",
+    "t": "heartbreak"
+  },
+  {
+    "c": `I spent 22 years with a lovely girl. I loved her but now I know I didn't love her in the way that I needed. When we broke up she told me that I didn't have any good memories of our time together. I do but I just don't express them well when face to face with tears. I still remember those early days in London. I still remember the look on your face when I smuggled those pills into your first rave. I still remember the way you used to kiss me amidst the lazers while high on E and I certainly remember the night that I played this song to you. Over and over again while you sat on my bed smoking joint after joint with me and laughing at how I sung this song to you, on loop, for what I remember to be an entire night of happiness that I still remember vividly to this day. I will love you till I die and I will.. but I am sorry it didn't take the pain away. At that moment I was confident it would. My heart was in the right place. I know for that night your heart was there too. Please don't think I don't have any happy memories of you.`, 
+    "v": "iB7E1D_3Na4",
+    "s": "Ladies and Gentlemen We Are Floating in Space – Spiritualized",
+    "t": "romantic"
+  },
+  {
+    "c": `For GARRETT, a guy from Colorado/Maryland who showed me this song and also showed me what it's like to truly connect with someone. Soul to soul, mind to mind, not like the other ones in the past. I miss your voice, your sleepy "hellos", and your giggles. I miss your words, and the way you put your thoughts together. I miss your little quirks, and even the ones that would annoy me sometimes. I hope that as you slipped away you felt what this songs you showed me made you feel, but also my love. I hope you are eternally jamming out. One day we will meet again, to tell you that I indeed love you too. Rest in my heart.`, 
+    "v": "iB7E1D_3Na4",
+    "s": "Ladies and Gentlemen We Are Floating in Space – Spiritualized",
+    "t": "romantic"
+  },
+  {
+    "c": `i first discovered this beautiful song with someone i loved, they shared it with me, and it felt so intimate and nice i fell asleep to it throught the phone. learned that she was in a relationship and my heart sunk. every now and then i come back here to listen to this masterpiece, remember the times she and i shared, remember how she'd take care of me. we're now friends, but my heart keeps wondering where we could've been now if she were mine.`, 
+    "v": "iB7E1D_3Na4",
+    "s": "Ladies and Gentlemen We Are Floating in Space – Spiritualized",
+    "t": "heartbreak"
+  },
+  {
+    "c": `Been hearing this song in my head thinking about the only person I've ever had mutual happiness with and loved more than anyone.
+
+A girl named Lucia. The highlight of my life. When we met, we were seeing each other at our worst, but we connected on a level I've never found in my life. We were both in bad situations but we found so much happiness in each other. From holding hands everywhere, to the little tug of war battles in the stores and elsewhere, to cuddling in bed to watch videos on her phone, to feeding ducks and swinging in the park, and so many more things, every one of them special to me.
+
+I was there for her since the moment we met, and I always will be.
+
+But I said too many dumb things, and things that got misunderstood. I got emotional over small things too much because of my past. I focused too much on being happy and making her happy, so much so that I neglected other priorities which would have helped us both, like making more money so we could move closer to her friends and family. Ultimately that led to her feeling trapped and isolated and emotionally wrecked.
+
+She's young and still figuring a lot of things out, so I don't blame her for anything. I understand the things that made her feel how she felt to leave. I do wish she hadn't let other people get in the way and second-guessed who I am though. She's the only person I've never told a lie to, but I couldn't convince her to trust and believe me over people who speak from a position of "authority" or who have their own motives. But with time, maybe eventually she'll realize these people were wrong.
+
+I wish I could just have a chance to fix myself and make up for my mistakes and try again. I would give anything for that.
+
+Now I feel like I don't matter at all. Every day I'm thinking of her. I wish I wasn't such a broken human being. If only I'd thought of the right words...
+
+I wonder if she misses me as much as I miss her.`, 
+    "v": "Rel8ZWkNNlM",
+    "s": "Pictures of You – The Cure",
+    "t": "heartbreak"
+  },  
+  {
+    "c": `You gave every song to me, then you gave the same songs to another. But hey: from life, you want to make alot of money and feel dead inside. Bon courage, c***.`, 
+    "v": "U43AA5meBXg",
+    "s": "Birds – Electrelane",
+    "t": "heartbreak"
+  },  
+  {
+    "c": `remember when you loved me in the morning. i miss you forever. goodbye my love`, 
+    "v": "U43AA5meBXg",
+    "s": "Birds – Electrelane",
+    "t": "romantic"
+  },  
+  {
+    "c": `He’ll never see this so I’m going to say it. This song makes me think of you and I love you, even if you’re too scared to say it back, it doesn’t matter to me`, 
+    "v": "ZDfXdsKwMMI",
+    "s": "Orange Peeler – Horse Jumper of Love",
+    "t": "romantic"
+  },
+  {
+    "c": `I played this song while I was having a car ride with my dad and he loved it and it kinda became our song
+he passed away a month ago and it hurts so much to listen to it but it brings so much memories
+rest in peace my love I'll miss you forever`, 
+    "v": "wjHgiSx0RNQ",
+    "s": "Robbers – The 1975",
+    "t": "familial"
+  },
+  {
+    "c": `I come back to this song every time I feel that strange need for reckless loving.
+
+I'm in a relationship and we're both very happy, very safe people. However, sometimes I want what this video shows... that extreme risk, and that reckless love. That jolt of excitement, that rush of a kiss after something insane just went down... I'd never risk my life, nor my partners for a thrill, but damn. This video makes me think.`, 
+    "v": "wjHgiSx0RNQ",
+    "s": "Robbers – The 1975",
+    "t": "longing"
+  },
+  {
+    "c": `My girlfriend and I really adored this song. It speaks a lot about our love, how we stood despite the differences we have from our family backgrounds, especially religion. Our family will despise us, people will, but guess that's what love is, to stand and fight together despite storms.
+
+No one can rob us of our love for each other, and no matter what people will say, she'll always look cool.`, 
+    "v": "wjHgiSx0RNQ",
+    "s": "Robbers – The 1975",
+    "t": "romantic"
+  },
+  {
+    "c": `Someone I loved with all my heart once sent me this song. It became a theme for us and it was beautiful. But time eroded those feelings and showed the true rock beneath and the love was untrue. For a long time I could not listen to this song without feeling such a great loss that my soul cried out. It still hurts, but I can endure it now. Amazing how much clarity pain can bring.`, 
+    "v": "ge3cf-AEZEs",
+    "s": "Boats and Birds – Gregory and the Hawk",
+    "t": "romantic"
+  },
+  {
+    "c": `“There’s a time where we get old and replaced the things we once loved with other things, when you realize the stuff you left behind in your childhood.. you remembered them and started crying. Nothing’s more important than love”`, 
+    "v": "MSepOYJxB64",
+    "s": "Wet Hands – C418",
+    "t": "longing"
+  },
+  {
+    "c": `I never new i could enjoy 10 hours of remembering my depressing past that i forgot about and also the horrible mistakes i had done that made me miserable and totally lonely and this helped me to relax and enjoy for i will have happiness until i think about my mistakes and sins that i did and i regret doing because it causes me more depression and sadness and heartbreak and tears that fallen from my eyes, my eyes that saw what i have done in the past that almost ruined my life, but i will never forget that no matter what will i do, someone is always there to forgive you.
+
+Me and my longest comment`, 
+    "v": "MSepOYJxB64",
+    "s": "Wet Hands – C418",
+    "t": "self"
+  },
+  {
+    "c": `Come. Have a seat. Don't worry, it's alright. You can stay here as long as you need. In the meantime just relax, get comfortable. I know it's been difficult these past few years but hey, at least we have this opportunity to watch the sun set over the horizon. It's been a long run. Maybe tomorrow we can do this again.`, 
+    "v": "MSepOYJxB64",
+    "s": "Wet Hands – C418",
+    "t": "self"
+  },
+  {
+    "c": `This was the first song that played when I played Minecraft with my dad the first time. I remember having so many good times with him, just fooling around, building things, and having fun. He committed suicide 5 years ago. When I started up one of our Minecraft worlds on our Xbox, I cried so hard. Just goes to show that even music, game music at that, leaves a mark on people.`, 
+    "v": "MSepOYJxB64",
+    "s": "Wet Hands – C418",
+    "t": "self"
+  },
+  {
+    "c": `I haven't cried in years. But this finally did it.`, 
+    "v": "MSepOYJxB64",
+    "s": "Wet Hands – C418",
+    "t": "self"
+  },
+  {
+    "c": `you hate it all and yet you still use shampoo..`, 
+    "v": "M_5ysvE_GiE",
+    "s": "Shampoo Suicide – Broken Social Scene",
+    "t": "self"
+  },
+  {
+    "c": `My ex husband dedicated this song to me. We separated years ago, but we still had a good bond because of the kids. My first love. My best friend. He recently passed away in October leaving behind 2 little girls, Emma & Lua. RIP Kyle, I’ll always be your yellow bird!`, 
+    "v": "54Z2zfr1mrI",
+    "s": "Poison Oak – Bright Eyes",
+    "t": "romantic"
+  },
+  {
+    "c": `This song reminds me of me and my brother fighting in our feild... He was much older would always win, and would leave me to walk home alone with bloody knees and hands... Now he's dead and I put his ashes out to that field... Not sure if i feel liberated or lonely...`, 
+    "v": "54Z2zfr1mrI",
+    "s": "Poison Oak – Bright Eyes",
+    "t": "familial"
+  },
+  {
+    "c": `Now I'm on the other side guys, love is beautiful,`, 
+    "v": "54Z2zfr1mrI",
+    "s": "Poison Oak – Bright Eyes",
+    "t": "self"
+  },
+  {
+    "c": `Me and my best friend spent a long time getting this right. He is dead now and this song makes me think of him and hurting`, 
+    "v": "54Z2zfr1mrI",
+    "s": "Poison Oak – Bright Eyes",
+    "t": "self"
+  },
+  {
+    "c": `I love her. I know it now. The distance scared me so I distanced myself from her and treated her special while seeing other people as just bodies to satiate that lust. But she's not just another face. Now I'm in too deep and I don't know if I can cut off the people I see as bodies after breaking her heart. I know I can but it's easier this way because I'm not hurting her I guess.
+I should leave the meaningless sex and flirting behind and love her, but I'm scared.
+She's not just another face. I'm scared I'll stop feeling this way and hurt her.`, 
+    "v": "WMrU3f5QQR8",
+    "s": "How Do I Tell A Girl I Want To Kiss Her? – Modern Baseball",
+    "t": "longing"
+  },
+  {
+    "c": `Story time kiddos! Y’all don’t have to read but if you do then I fucking applaud you for it. I’m 19 now and high school feels like a distant memory now but it’s also all very fresh. I was best friends with the same girl all the way from back in second grade and we talked constantly and it was fucking great. Along comes sophomore year and my parents divorced, and of course me already suffering from depression went into a whole tail spin. When my mom and I moved into our new apartment the previously mentioned friend helped me move my stuff into my new room in the attic when I finally and genuinely asked her if she wanted to be together and she said yes with this giggle that lit up my world and this look in her eyes that melted my heart. A week later we shared our first kiss and it is a moment that has always been vivid in my mind and I cherish it every day. As time passed we spent every moment we could together along with sordid friends and people from clubs at school, she was in marching band so of course I went to every game so I could see her play and I was in musicals so she always came to my shows. Fast forward to today when I wish I could say this has a happy ending. That we are still together and moving in and things are still amazing but no. Painfully that’s not the case. The fact that I just found this song reminded me of just how bad I fucked it all up. After two and a half years I left her and it was the dumbest thing I ever did. It was two weeks from prom after she already said yes, a week after my senior show where I told her backstage that everything would be ok, a month after making plans to be together forever. Like an idiot I broke it off and whether it was out of fear or a need to get away for a while I don’t know. We still talk and we are at the same college now. Things aren’t the same and I acted like an asshole for a while after which only hurt her more. Now she’s with a new guy and they are getting along amazingly and I’m happy for her. Even though it means that even if I wanted we can’t hang out really cause she’s too busy between work and the boy friend. Like i said though, not a day goes by where I don’t think of her or her smile, that giggle, listening to her sing while I’m pretending to sleep on a road trip, swimming in the summer, vacations in the south, naps after school, Christmas Eve in each other’s arms, it’s all still there and it hurts but it’s beautiful. So yea that’s a thing. Enjoy the song everyone, keep listening to music it’s a helluva thing for us all.`, 
+    "v": "WMrU3f5QQR8",
+    "s": "How Do I Tell A Girl I Want To Kiss Her? – Modern Baseball",
+    "t": "longing"
+  },
+  {
+    "c": `This songs remains me of a girl I used to be in love with. We were on the same class room during the whole high school, it was not until the second semester when we started to talk. She was everything I dreamt before, then I had this friend who managed to exchange Facebook passwords with her so there was a conversation of her telling her best friend that she had a crush on me, I was the lonely teenager that used to be drunk every weekend while blasting hardcore stuff with his friends, to read that chat was like "damn, I'm actually something for someone else that it's not my mom". We spent whole nights talking/texting and then we looked like zombies the day after and since we always sat next to each other I remember my hands always touching her hair lol I loved her more than anything, I always told her that I loved her but for some strange reason even though I knew she felt something she never wanted to be something more, high school was over and I was supposed to kiss her the last day but her mom came to pick her up and everything went to shit. Some days later she was having her birthday, I wrote her but I got some fucked up shit in response then I decided that it was not healthy for me to love her, we stopped talking and we became strangers to each other, couple years ago I met her in the downtown but again we just passed over each other like we never met before, I still think about her even tho it's 4 years now since we left each other. I wonder if she remembers...`, 
+    "v": "WMrU3f5QQR8",
+    "s": "How Do I Tell A Girl I Want To Kiss Her? – Modern Baseball",
+    "t": "longing"
+  },
+  {
+    "c": `I always end up listening to this song, but i never realised till about a week ago that what is going on in this song is actually happening to me in real life. I need this girl more than she needs me, and im constantly thinking about her, but I know i'll never be able to go there from what happened in the past. Shes like my rock and has been since day 1. Shes amazing, but everything between us sucks now.`, 
+    "v": "WMrU3f5QQR8",
+    "s": "How Do I Tell A Girl I Want To Kiss Her? – Modern Baseball",
+    "t": "longing"
+  },
+  {
+    "c": `my crush kissed me last night and literally this song came through my head because he was like " and what if i tell you that i really wanna kiss you " and it was so cute i just can't ughhhhhhh
+be happy with me pls
+`, 
+    "v": "WMrU3f5QQR8",
+    "s": "How Do I Tell A Girl I Want To Kiss Her? – Modern Baseball",
+    "t": "romantic"
   }
   
+  
+  
+  
+  
+  
+  
+  
+  
 ]
+
+let c = 0;
+
+console.log(love.length);
+
+$(document).click(function (e) {
+
+  if (e.button == 0) {
+    
+    let r = Math.floor(Math.random()*(love.length));
+
+    if(c === 0){
+      $(".__intro").fadeOut(4000);
+      $("*").css("cursor", "wait");
+      setTimeout(
+        function() 
+        {
+      $("*").css("cursor", "pointer");
+            $("#player").css("opacity", 1);
+            $("p.isaid").fadeIn(3000);
+        
+            $("p.isaid").text(love[r].c);
+            player.loadVideoById(love[r].v);
+          
+        }, 4000);
+      
+    } else{
+      
+    thesoundof(r);
+    }
+    
+    c++;
+    
+    //    console.log(c + ": " + when[c]);
+    
+
+  }
+  
+  
+});
+
+
+function thesoundof(r){
+//  $(love[r].c).hide().prependTo("p.isaid").fadeIn(4000);
+      $("*").css("cursor", "wait");
+//  $("p.isaid").fadeOut(1000);
+//  $("p.isaid").text(love[r].c);
+  $("p.isaid").fadeOut(function() {
+  $(this).text(love[r].c).fadeIn(2000);
+});
+        
+  $("#player").css("opacity", 0);
+      setTimeout(
+        function() 
+        {
+      $("*").css("cursor", "pointer");
+            $("#player").css("opacity", 1);
+        }, 1500);
+  
+  player.loadVideoById(love[r].v);
+}
